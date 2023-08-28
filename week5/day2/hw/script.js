198150a54c35a5864ba1ac7c7daa66a5990437c7
Shtraimish <shtraimish1@gmail.com>
2023-08-29 00:20:40 +0300

    week5d2HW

--- week5/day2/hw/script.js
+++ week5/day2/hw/script.js
@@ -9,8 +9,6 @@
 //     });
 // }
 
-
-
 // //In the example, the promise should reject
 // compareToTen(15)
 //   .then(result => console.log(result))
@@ -21,3 +19,15 @@
 //   .then(result => console.log(result))
 //   .catch(error => console.log(error))
 
+//ex2
+function sucsessWaiter() {
+  return new Promise((resolve, reject) => {
+    console.log("before");
+    setTimeout( ()=> {
+      console.log("inside");
+      resolve("sakis");
+    }, 4000);
+  
+  });
+}
+sucsessWaiter();
